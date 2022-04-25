2d5377ae7e590ffe0f73d29b9cd31e1a84ffc541
japesh <japeshsingla1@gmail.com>
2022-04-25 21:30:00 +0530

    remove quote from varun

--- src/components/Quotes/Quotes.tsx
+++ src/components/Quotes/Quotes.tsx
@@ -13,55 +13,29 @@ const quotesList = [
     title: "Chief Technology Officer and President of Digital",
     image: "/img/exec-photo.png",
   },
-  {
-    quote:
-      "At Fox, we provide employees with the culture of a startup, offering each team member the opportunity to make a difference in defining what's next for Fox Tech.",
-    name: "Varun Narang",
-    title: "Chief Product Officer",
-    image: "/img/exec-photo-2.png",
-  },
 ];
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  initialSlide: 0,
-  autoplay: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-      },
-    },
-  ],
-};
 
 export default function Quotes() {
   return (
     <div className={clsx("container", Styles.container)}>
-      <Slider {...settings}>
-        {quotesList.map(({ quote, image, name, title }) => {
-          return (
-            <div className="padding-horiz--lg">
-              <h3 className={Styles.quoteMessage}>{quote}</h3>
-              <div className="padding-top--md avatar">
-                <img
-                  className="avatar__photo avatar__photo--lg"
-                  alt={`${title} exec photo`}
-                  src={image}
-                />
-                <div className="avatar__intro">
-                  <div className="avatar__name">{name}</div>
-                  <small className="avatar__subtitle">{title}</small>
-                </div>
+      {quotesList.map(({ quote, image, name, title }) => {
+        return (
+          <div className="padding-horiz--md">
+            <h3 className={Styles.quoteMessage}>{quote}</h3>
+            <div className="padding-top--md avatar">
+              <img
+                className="avatar__photo avatar__photo--lg"
+                alt={`${title} exec photo`}
+                src={image}
+              />
+              <div className="avatar__intro">
+                <div className="avatar__name">{name}</div>
+                <small className="avatar__subtitle">{title}</small>
               </div>
             </div>
-          );
-        })}
-      </Slider>
+          </div>
+        );
+      })}
     </div>
   );
 }
